9ad421eefd95f4597e04e6ca3fd84f162aa173b6
Rodrigo <rodrigocid2002@gmail.com>
2024-10-03 13:07:40 +0200

    YOKO LAVIADA PATROCINA ESTE COMMIT

--- app/src/main/java/com/example/bbva/intents/IntentExplicito.java
+++ app/src/main/java/com/example/bbva/intents/IntentExplicito.java
@@ -1,9 +1,10 @@
-package com.example.bbva;
+package com.example.bbva.intents;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.bbva.intents.IntentExplicito;
+import com.example.bbva.R;
 
-public class MainActivity extends AppCompatActivity {
+public class IntentExplicito extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intent_explicito);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //OBTENER BOTON DE LA VISTA
-        Button b = findViewById(R.id.buttonDINERO);
+
+        Button b = findViewById(R.id.bImplicito);
+        EditText tfComida = findViewById(R.id.tfComida);
+        EditText tfBebida = findViewById(R.id.tfBebida);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent iExp = new Intent(MainActivity.this, IntentExplicito.class);
-                startActivity(iExp);
-
+                Intent iImp = new Intent(IntentExplicito.this, IntentImplicito.class);
+                iImp.putExtra("Comida",tfComida.getText().toString());
+                iImp.putExtra("Bebida",tfBebida.getText().toString());
+                startActivity(iImp);
             }
         });
     }
--- app/src/main/java/com/example/bbva/intents/IntentImplicito.java
+++ app/src/main/java/com/example/bbva/intents/IntentImplicito.java
@@ -1,9 +1,8 @@
-package com.example.bbva;
+package com.example.bbva.intents;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +10,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.bbva.intents.IntentExplicito;
+import com.example.bbva.R;
 
-public class MainActivity extends AppCompatActivity {
+public class IntentImplicito extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intent_implicito);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //OBTENER BOTON DE LA VISTA
-        Button b = findViewById(R.id.buttonDINERO);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        TextView tComida = findViewById(R.id.lblComida);
+        TextView tBebida = findViewById(R.id.lblBebida);
 
-                Intent iExp = new Intent(MainActivity.this, IntentExplicito.class);
-                startActivity(iExp);
 
-            }
-        });
+
+
+        Bundle b = getIntent().getExtras();
+
+        //String prueba = b.getString("Prueba");
+
+
+        tComida.setText(b.getString("Comida"));
+        tBebida.setText(b.getString("Bebida"));
+
     }
 }
